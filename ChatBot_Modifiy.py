09ba5656adcf9cd66bbd26b109d337873a5dbc3e
chpark1358 <chpark425@gmail.com>
2024-08-26 16:01:38 +0900

    Update ChatBot_Modifiy.py

--- ChatBot_Modifiy.py
+++ ChatBot_Modifiy.py
@@ -5,11 +5,11 @@ from loguru import logger
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import Docx2txtLoader
-from langchain.document_loaders import UnstructuredPowerPointLoader
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import UnstructuredMarkdownLoader
+from from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
