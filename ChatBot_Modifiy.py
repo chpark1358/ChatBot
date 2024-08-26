26a6d77b4b6c053f4181081fb103dd262ed2aa6c
chpark1358 <chpark425@gmail.com>
2024-08-26 15:53:14 +0900

    Update ChatBot_Modifiy.py

--- ChatBot_Modifiy.py
+++ ChatBot_Modifiy.py
@@ -5,7 +5,11 @@ from loguru import logger
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
-from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader, TextLoader, UnstructuredMarkdownLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredMarkdownLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -13,18 +17,19 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.vectorstores import FAISS
 
+# from streamlit_chat import message
 from langchain.callbacks import get_openai_callback
 from langchain.memory import StreamlitChatMessageHistory
 
 # OpenAPI Key
-# openai_api_key = st.secrets["OPENAI_API_KEY"]
+#openai_api_key = st.secrets["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"]
 
 icon_path = r"C:\Users\tech\Downloads\Bear_test.jpg"
 
 def main():
     st.set_page_config(
-        page_title="Officekeeper ChatBot",
-        page_icon=icon_path)
+    page_title="Officekeeper ChatBot",
+    page_icon=icon_path)
 
     st.title("_OfficeKeeper :red[QA Chat]_")
 
@@ -38,23 +43,24 @@ def main():
         st.session_state.processComplete = None
 
     with st.sidebar:
-        uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx', 'pptx', 'txt', 'md'], accept_multiple_files=True)
+        uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx', 'pptx', 'txt', 'md'],accept_multiple_files=True)
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         process = st.button("Process")
-
     if process:
         if not openai_api_key:
             st.info("Please add your OpenAI API key to continue.")
             st.stop()
         files_text = get_text(uploaded_files)
         text_chunks = get_text_chunks(files_text)
-        vectorstore = get_vectorstore(text_chunks)
+        vetorestore = get_vectorstore(text_chunks)
+     
+        st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
 
-        st.session_state.conversation = get_conversation_chain(vectorstore, openai_api_key)
         st.session_state.processComplete = True
 
     if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "assistant", "content": "주어진 문서에 대해 질문해주세요"}]
+        st.session_state['messages'] = [{"role": "assistant", 
+                                        "content": "주어진 문서에 대해 질문해주세요"}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -73,17 +79,21 @@ def main():
             chain = st.session_state.conversation
 
             with st.spinner("Thinking..."):
+                result = chain({"question": query})
                 with get_openai_callback() as cb:
-                    result = chain({"question": query})
                     st.session_state.chat_history = result['chat_history']
                 response = result['answer']
-                source_documents = result.get('source_documents', [])
+                source_documents = result['source_documents']
 
                 st.markdown(response)
                 with st.expander("참고 문서 확인"):
-                    for doc in source_documents:
-                        st.markdown(doc.metadata['source'], help=doc.page_content)
+                    st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
+                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
+                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
+                    
 
+
+# Add assistant message to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
 def tiktoken_len(text):
@@ -92,65 +102,70 @@ def tiktoken_len(text):
     return len(tokens)
 
 def get_text(note):
-    keeper_list = []
 
+    keeper_list = []
+    
     for keeper in note:
-        file_name = keeper.name
-        with open(file_name, "wb") as file:
+        file_name = keeper.name  # keeper 객체의 이름을 파일 이름으로 사용
+        with open(file_name, "wb") as file:  # 파일을 keeper.name으로 저장
             file.write(keeper.getvalue())
             logger.info(f"Uploaded {file_name}")
-
-        if file_name.endswith('.pdf'):
+    
+        if '.pdf' in keeper.name:
             loader = PyPDFLoader(file_name)
-        elif file_name.endswith('.docx'):
+            documents = loader.load_and_split()
+        elif '.docx' in keeper.name:
             loader = Docx2txtLoader(file_name)
-        elif file_name.endswith('.pptx'):
+            documents = loader.load_and_split()
+        elif '.pptx' in keeper.name:
             loader = UnstructuredPowerPointLoader(file_name)
-        elif file_name.endswith('.txt'):
+            documents = loader.load_and_split()
+        elif '.txt' in keeper.name:
             loader = TextLoader(file_name)
-        elif file_name.endswith('.md'):
+            documents = loader.load_and_split()
+        elif '.md' in keeper.name:
             loader = UnstructuredMarkdownLoader(file_name)
-        else:
-            continue
+            documents = loader.load_and_split()
 
-        documents = loader.load_and_split()
         keeper_list.extend(documents)
-
     return keeper_list
+
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
         pattern=r"(#+\s)",
         chunk_size=900,
         chunk_overlap=100,
-        length_function=tiktoken_len,
-        keep_separator=True
+        length_function=tiktoken_len
     )
     chunks = text_splitter.split_documents(text)
     return chunks
 
+
 def get_vectorstore(text_chunks):
     embeddings = HuggingFaceEmbeddings(
-        model_name="jhgan/ko-sroberta-multitask",
-        model_kwargs={'device': 'cpu'},
-        encode_kwargs={'normalize_embeddings': True}
-    )
+                                        model_name="jhgan/ko-sroberta-multitask",
+                                        model_kwargs={'device': 'cpu'},
+                                        encode_kwargs={'normalize_embeddings': True}
+                                        )  
     vectordb = FAISS.from_documents(text_chunks, embeddings)
     return vectordb
 
-def get_conversation_chain(vectorstore, openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-4o', temperature=0)
+def get_conversation_chain(vetorestore,openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-4o',temperature=0)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        chain_type="stuff",
-        retriever=vectorstore.as_retriever(search_type='mmr', verbose=True),
-        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
-        get_chat_history=lambda h: h,
-        return_source_documents=True,
-        verbose=True
-    )
+            llm=llm, 
+            chain_type="stuff", 
+            retriever=vetorestore.as_retriever(search_type = 'mmr', vervose = True), 
+            memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+            get_chat_history=lambda h: h,
+            return_source_documents=True,
+            verbose = True
+        )
 
     return conversation_chain
+
+
 
 if __name__ == '__main__':
     main()
